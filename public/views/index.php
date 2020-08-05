3739b7ff9deec98610e76d49ca8e0e7c2137358d
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-08-05 19:19:07 -0400

    initial home page format

---
+++ public/views/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weight Loss Tracker</title>
+</head>
+<body>
+    <main>
+        <form  action="">
+            <label for="student_num">Calorie Intake</label>
+            <input id='student_num' inputmode="numeric" placeholder="*required" required/>
+
+            <!-- DISABLE BUTTON ONCE CLICKED-->
+            <input type="submit" value="Input Calorie">
+        </form>
+        <form  action="">
+            <label for="team">Weight</label>
+            <input id='team' type="text" placeholder="*required" required/>
+
+            <!-- DISABLE BUTTON ONCE CLICKED-->
+            <input type="submit" value="Input Weight">
+        </form>
+        <form  action="">
+            <label for="team">Food Eaten</label>
+            <input id='team' type="text" placeholder="*required" required/>
+
+            <!-- DISABLE BUTTON ONCE CLICKED-->
+            <input type="submit" value="Input Food Eaten">
+        </form>
+    </main>
+</body>
+</html>
