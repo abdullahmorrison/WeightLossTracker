303ce879f3737bb1293b298134db149e2f256a2f
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-08-06 19:09:06 -0400

    added header footer and organized form tags

--- public/views/index.php
+++ public/views/index.php
@@ -3,31 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="styles.css?"/><!--STYLES-->
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"><!--FONTS-->
+
     <title>Weight Loss Tracker</title>
 </head>
 <body>
+    <header>
+        <h1>Weight Loss Tracker</h1>
+    </header>
     <main>
-        <form  action="">
+        <form  action="" method="post">
             <label for="student_num">Calorie Intake</label>
-            <input id='student_num' inputmode="numeric" placeholder="*required" required/>
+            <input id='student_num' inputmode="numeric" placeholder="*required" required/>  
+            <input type="submit" value="Input Calorie"/><!-- DISABLE BUTTON ONCE CLICKED-->
 
-            <!-- DISABLE BUTTON ONCE CLICKED-->
-            <input type="submit" value="Input Calorie">
-        </form>
-        <form  action="">
-            <label for="team">Weight</label>
-            <input id='team' type="text" placeholder="*required" required/>
-
-            <!-- DISABLE BUTTON ONCE CLICKED-->
-            <input type="submit" value="Input Weight">
-        </form>
-        <form  action="">
             <label for="team">Food Eaten</label>
             <input id='team' type="text" placeholder="*required" required/>
+            <input type="submit" value="Input Food Eaten"/><!-- DISABLE BUTTON ONCE CLICKED-->
 
-            <!-- DISABLE BUTTON ONCE CLICKED-->
-            <input type="submit" value="Input Food Eaten">
+            <label for="team">Weight</label>
+            <input id='team' type="text" placeholder="*required" required/>
+            <input type="submit" value="Input Weight"/><!-- DISABLE BUTTON ONCE CLICKED-->
         </form>
     </main>
+    <footer>
+        Contact: abdullahmorrison@gmail.com
+    </footer>
 </body>
 </html>
+<?php
+    if ($_GET) {
+        if (isset($_POST['insert'])) {
+            echo "worked";
+        } else {
+            echo "fail";
+        }
+    }
+?>
