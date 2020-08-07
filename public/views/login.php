64bd48e9d154d35ab1204194c741c48ed9b608e7
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-08-07 17:25:51 -0400

    created file for login

---
+++ public/views/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="styles.css?"/><!--STYLES-->
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"><!--FONTS-->
+
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <title>Login</title>
+</head>
+<body>
+    Hi
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+</body>
+</html>
+<!--
+ClientID:
+586315839113-25vk6s0n1un7gpmfh3au9750gpotjmai.apps.googleusercontent.com
+CLientSecret:
+SAanR5VD9-ydb3QOsGR1j4pZ
