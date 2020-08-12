a6e38b8fb12640599e3c06abf129138ab1a5b663
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-08-12 19:12:47 -0400

    made a login page

--- public/views/login.php
+++ public/views/login.php
@@ -8,16 +8,43 @@
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"><!--FONTS-->
 
     <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id" content="586315839113-25vk6s0n1un7gpmfh3au9750gpotjmai.apps.googleusercontent.com">
+
     <title>Login</title>
 </head>
 <body>
-    Hi
-    <div class="g-signin2" data-onsuccess="onSignIn"></div>
-
+    <?php include('header.php'); ?>
+    <main>
+        <form>
+            <div class="g-signin2" data-onsuccess="onSignIn">Google Sign In</div>
+            <!--<a href="#" onclick="signOut();">Sign out</a>-->
+            <script>
+                if (auth2.isSignedIn.get()) {
+                    var profile = auth2.currentUser.get().getBasicProfile();
+                    console.log('ID: ' + profile.getId());
+                    console.log('Full Name: ' + profile.getName());
+                    console.log('Given Name: ' + profile.getGivenName());
+                    console.log('Family Name: ' + profile.getFamilyName());
+                    console.log('Image URL: ' + profile.getImageUrl());
+                    console.log('Email: ' + profile.getEmail());
+                }
+            </script>
+        </form>
+    </main>
 </body>
 </html>
-<!--
-ClientID:
-586315839113-25vk6s0n1un7gpmfh3au9750gpotjmai.apps.googleusercontent.com
-CLientSecret:
-SAanR5VD9-ydb3QOsGR1j4pZ
+<script>
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+    function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+        console.log('User signed out.');
+        });
+    }
+</script>
