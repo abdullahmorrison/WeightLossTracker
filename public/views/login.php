f080e90be9ec0ddeff58a515bc7e71cab8314b99
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-08-14 17:36:02 -0400

    made ability to login with google (incomplete)

--- public/views/login.php
+++ public/views/login.php
@@ -10,6 +10,22 @@
     <script src="https://apis.google.com/js/platform.js" async defer></script>
     <meta name="google-signin-client_id" content="586315839113-25vk6s0n1un7gpmfh3au9750gpotjmai.apps.googleusercontent.com">
 
+    <script>
+        function onSignIn(googleUser) {
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        }
+        function signOut() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+                console.log('User signed out.');
+            });
+        }
+    </script>
+
     <title>Login</title>
 </head>
 <body>
@@ -17,34 +33,8 @@
     <main>
         <form>
             <div class="g-signin2" data-onsuccess="onSignIn">Google Sign In</div>
-            <!--<a href="#" onclick="signOut();">Sign out</a>-->
-            <script>
-                if (auth2.isSignedIn.get()) {
-                    var profile = auth2.currentUser.get().getBasicProfile();
-                    console.log('ID: ' + profile.getId());
-                    console.log('Full Name: ' + profile.getName());
-                    console.log('Given Name: ' + profile.getGivenName());
-                    console.log('Family Name: ' + profile.getFamilyName());
-                    console.log('Image URL: ' + profile.getImageUrl());
-                    console.log('Email: ' + profile.getEmail());
-                }
-            </script>
+            <a href="#" onclick="signOut();">Sign out</a>
         </form>
     </main>
 </body>
 </html>
-<script>
-    function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    }
-    function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-        console.log('User signed out.');
-        });
-    }
-</script>
