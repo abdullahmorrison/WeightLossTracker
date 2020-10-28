6bb9369f020d2aa3bdc0ee673a5b3aa3e84ca85f
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-10-28 13:10:26 -0400

    made to display the food

--- private/database/fetchQueries.php
+++ private/database/fetchQueries.php
@@ -1,15 +1,24 @@
 <?php
 include('config.php');
 
-function getFood($conn){
+/**
+ * Description: This function retrieves all data form the food table.
+ * @param food_id: if this is set (not null) it will only retieve the food data from that food_id
+ */
+function getFood($conn, $food_id = null)
+{
     // !REFACTOR AND COMMENT
     $food = array();
-    $res = $conn->query("SELECT * From food");
+    if ($food_id != null) {
+        $res = $conn->query("SELECT * From food WHERE food_id = $food_id");
+    } else {
+        $res = $conn->query("SELECT * From food");
+    }
     if ($res) {
-        while($rows = mysqli_fetch_assoc($res)){
+        while ($rows = mysqli_fetch_assoc($res)) {
             array_push(
                 $food,
-                array($rows['food_name'], $rows['calories'])
+                array($rows['food_id'], $rows['food_name'], $rows['calories'])
             );
         }
     } else {
