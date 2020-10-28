116d2e1c7d426b145c2996db5fe8e62bad0f5635
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-10-28 09:55:59 -0400

    removed question feature for more minimalization

--- public/views/pageComponents/header.php
+++ public/views/pageComponents/header.php
@@ -1,7 +1,6 @@
 <header>
     <ul>
         <li><h1><a href="index.php">Weight Loss Tracker</a></h1></li>
-        <li><a href="question.php">Ask Question</a></li>
         <li><a href="graphs.php">Graphed Data</a></li>
         <li><a href="">Login</a></li>
     </ul>
