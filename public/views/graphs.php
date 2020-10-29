741190cc710c850191f6b14299f48dd2b8a00de2
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-10-29 11:28:22 -0400

    displayed data on line graph

--- public/views/graphs.php
+++ public/views/graphs.php
@@ -7,38 +7,37 @@
     <link rel="stylesheet" href="styles.css"/><!--STYLES-->
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"><!--FONTS-->
 
+    <?php
+        include('../../private/database/fetchQueries.php');
+    ?>
+
     <title>Graphed Data</title>
 </head>
 <body>
     <?php include('pageComponents/header.php'); ?>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <canvas id="myChart" width="400" height="150"></canvas>
+    <?php
+        $food = getFood($conn); //getting all food from database
+        $foodCalories = array();
+        $foodDate = array();
+        foreach ($food as $f) { //looping through each food
+            //food date eaten (f[3]) and its calories (f[2])
+            array_push($foodDate, "'".$f[3]."'"); //x-axis
+            array_push($foodCalories, $f[2]); //y-axis
+        }
+        $foodDateString = implode(', ' , $foodDate);
+        $foodCaloriesString = implode(', ' , $foodCalories);
+    ?>
     <script>
         var ctx = document.getElementById('myChart');
         var myChart = new Chart(ctx, {
-            type: 'bar',
+            type: 'line',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: [<?php echo $foodDateString ?>],
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
+                    label: 'Calories',
+                    data: [<?php echo $foodCaloriesString ?>],
                 }]
             },
             options: {
