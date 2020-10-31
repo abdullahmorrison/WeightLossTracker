bbbd18d45c2303eaad98290cb2a137ad6410d048
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-10-31 12:31:06 -0400

    added title and moved graph into a container

--- public/views/graphs.php
+++ public/views/graphs.php
@@ -16,49 +16,55 @@
 <body>
     <?php include('pageComponents/header.php'); ?>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <canvas id="myChart" width="400" height="150"></canvas>
-    <?php
-        $food = getFood($conn); //getting all food from database
-        $foodCalories = array();
-        $foodDate = array();
-        foreach ($food as $f) { //looping through each food
-            //food date eaten (f[3]) and its calories (f[2])
-            array_push($foodDate, "'".$f[3]."'"); //x-axis
-            array_push($foodCalories, $f[2]); //y-axis
-        }
-        $foodDateString = implode(', ' , $foodDate);
-        $foodCaloriesString = implode(', ' , $foodCalories);
-    ?>
-    <script>
-        var ctx = document.getElementById('myChart');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: [<?php echo $foodDateString ?>],
-                datasets: [{
-                    label: 'Calories',
-                    data: [<?php echo $foodCaloriesString ?>],
-                    backgroundColor: 'rgb(255, 0, 0)',
-                    fill: false,
-                    lineTension: 0,
-                    pointRadius: 10,
-                    pointHoverRadius: 12
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
+    <div class="graphContainer">
+        <canvas id="myChart" width="400" height="200"></canvas>
+        <?php
+            $food = getFood($conn); //getting all food from database
+            $foodCalories = array();
+            $foodDate = array();
+            foreach ($food as $f) { //looping through each food
+                //food date eaten (f[3]) and its calories (f[2])
+                array_push($foodDate, "'".$f[3]."'"); //x-axis
+                array_push($foodCalories, $f[2]); //y-axis
+            }
+            $foodDateString = implode(', ' , $foodDate);
+            $foodCaloriesString = implode(', ' , $foodCalories);
+        ?>
+        <script>
+            var ctx = document.getElementById('myChart');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: [<?php echo $foodDateString ?>],
+                    datasets: [{
+                        label: 'Calories',
+                        data: [<?php echo $foodCaloriesString ?>],
+                        backgroundColor: 'rgb(255, 0, 0)',
+                        fill: false,
+                        lineTension: 0,
+                        pointRadius: 10,
+                        pointHoverRadius: 12
                     }]
                 },
-                legend: {
-                    display: false
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    },
+                    title: {
+                        display: true,
+                        text: 'Calories Eaten within a day'
+                    },
+                    legend: {
+                        display: false
+                    }
                 }
-            }
-        });
-    </script>
+            });
+        </script>
+    </div>
     <?php include('pageComponents/footer.php'); ?>
 </body>
 </html>
